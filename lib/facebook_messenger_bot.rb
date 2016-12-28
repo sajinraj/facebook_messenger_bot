7724c4fe48541ae2ec43383a1f0604e9f7edb5ab
suslov <suslov@qburst.com>
2016-12-28 17:37:21 +0530

    initial gem setup

--- lib/facebook_messenger_bot.rb
+++ lib/facebook_messenger_bot.rb
@@ -1,5 +1,6 @@
 
 module FacebookMessengerBot
+  extend self
   require "facebook_messenger_bot/version"
   require 'facebook_messenger_bot/messenger_one_time_apis'
   require 'facebook_messenger_bot/element_hashes'
